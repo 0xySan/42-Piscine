5ec64dad0140943124febc2c4d16a9690a1db292
= <=>
2024-08-07 15:39:42 +0200

    finished print numbers

---
+++ c00/ex03/ft_print_numbers.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etaquet <etaquet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/07 15:35:28 by etaquet           #+#    #+#             */
+/*   Updated: 2024/08/07 15:38:58 by etaquet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_numbers(void)
+{
+	char	number;
+
+	number = '0';
+	while (number <= '9')
+	{
+		write(1, &number, 1);
+		number++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_numbers();
+	return (0);
+}
